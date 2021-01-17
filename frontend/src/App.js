62d4fe88f88a4f731158c6bc408c12f155842b91
Ben <benlaugherty@gmail.com>
2021-01-16 17:01:45 -0700

    add cleaned up frontend for building web view of the image repository

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Text>Mosaic</Text>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
